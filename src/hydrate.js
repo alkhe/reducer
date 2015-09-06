5f85a2b729f28fd49540511d17ccda8b87a55b13
edge <sort@programmer.net>
2015-09-06 12:39:17 +0900

    fix type check in Hydrate

--- src/hydrate.js
+++ src/hydrate.js
@@ -2,6 +2,6 @@ export let type = '@reducer:HYDRATE';
 
 export default store =>
 	(state, action) =>
-		(action !== undefined) && (action.type === Hydrate.type)
+		(action !== undefined) && (action.type === type)
 			? action.state
 			: store(state, action);
